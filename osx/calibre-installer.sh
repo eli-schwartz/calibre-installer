e6fc5bb0a1affc83ac0bc8598a7948ef3e60dcd0
Eli Schwartz <eschwartz93@gmail.com>
2014-12-09 13:38:38 -0600

    Recycle linux code for OSX, still needs some work.

---
+++ osx/calibre-installer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Set the location of calibre-upgrade and launchd plist.
+upgrade_script=https://github.com/eli-schwartz/calibre-installer/raw/master/osx/calibre-upgrade.sh
+launchd_plist=https://github.com/eli-schwartz/calibre-installer/raw/master/osx/com.calibre.updater.plist
+
+# Functions
+
+do_install()
+{
+    wget -nv -O /usr/bin/calibre-upgrade.sh $upgrade_script
+    chmod 755 /usr/bin/calibre-upgrade.sh
+}
+
+add_launchd()
+{
+    echo "Installing launchd global daemon..."
+    wget -nv -O /Library/LaunchDaemons/com.calibre.updater.plist $launchd_plist
+    chmod 644 /Library/LaunchDaemons/com.calibre.updater.plist
+}
+
+usage()
+{
+	cat <<- _EOF_
+		Usage: calibre-installer.sh [OPTIONS]
+		Installs the calibre-upgrade command and creates a Launchd daemon to regularly update calibre.
+
+		OPTIONS
+		    -h, --help        Shows this help message.
+_EOF_
+}
+
+# Options
+while [ "$1" != "" ]; do
+    case $1 in
+        -h|--help)      usage
+                        exit
+                        ;;
+        *)              echo "calibre-installer.sh: unrecognized option '$1'"
+                        echo "Try 'calibre-installer.sh --help' for more information."
+                        exit 1
+    esac
+    shift
+done
+
+# Main
+
+## Check that we are running as root
+if [[ $EUID -ne 0 ]]; then
+    echo -e "You can only install calibre if you have root permission."
+    exit 1
+fi
+
+do_install
+add_launchd
---
+++ osx/calibre-upgrade.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Unset git development source
+export CALIBRE_DEVELOP_FROM=
+# by default, switch is off
+force_upgrade=0
+
+
+# Functions
+
+calibre_is_installed()
+{
+    command -v calibre >/dev/null 2>&1
+}
+
+usage()
+{
+	cat <<- _EOF_
+		Usage: calibre-upgrade.sh [OPTIONS]
+		Upgrades calibre installation. Automatically checks if the current version is up to date.
+		Must be run as root.
+
+		OPTIONS
+		    -f, --force       Force an update. This is only useful if binaries
+		                      were updated for a critical error. :shrug:
+		    -h, --help        Displays this help message.
+_EOF_
+}
+
+do_upgrade()
+{
+    if calibre_is_installed; then
+        # shutdown calibre as each logged-in user.
+        for i in $(users | tr ' ' '\n' | sort -u); do
+            sudo -u $i calibre --shutdown-running-calibre
+        done
+        killall -q -v calibre-server && echo -e "Restart when upgrade is finished. ;)\n\n" || echo -e "No running calibre servers.\n\n"
+    fi
+    wget -nv -O- https://github.com/kovidgoyal/calibre/raw/master/setup/linux-installer.py | python -c "import sys; main=lambda x,y:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+}
+
+# Options
+while [ "$1" != "" ]; do
+    case $1 in
+        -h|--help)      usage
+                        exit
+                        ;;
+        -f|--force)     shift
+                        force_upgrade=1
+                        ;;
+        *)              echo "calibre-upgrade.sh: unrecognized option '$1'"
+                        echo "Try 'calibre-upgrade.sh --help' for more information."
+                        exit 1
+    esac
+    shift
+done
+
+# Main
+
+## Check that we are running as root
+if [[ $EUID -ne 0 ]]; then
+    echo -e "You can only install calibre if you have root permission."
+    exit 1
+fi
+
+
+
+if calibre_is_installed; then
+    calibre-debug -c "import urllib as u; from calibre.constants import numeric_version; raise SystemExit(int(numeric_version  < (tuple(map(int, u.urlopen('http://calibre-ebook.com/downloads/latest_version').read().split('.'))))))"
+    UP_TO_DATE=$?
+else
+    echo -e "Calibre is not installed, installing...\n\n"
+    UP_TO_DATE=1
+fi
+
+if [ "$UP_TO_DATE" = 0 ]; then
+    echo "Calibre is up-to-date"
+
+    if [ "$force_upgrade" = 1 ]; then
+        echo ""
+        echo "Forcing upgrade anyway -- are you sure you want to continue? [y/n]"
+        read answer
+
+        if [[ "$answer" = "y" || "$answer" = "yes" ]]; then
+            do_upgrade
+        else
+            echo "Exiting..."
+            exit 1
+        fi
+    fi
+else
+    calibre_is_installed && echo -e "Calibre is out-of-date. Upgrading...\n\n"
+    do_upgrade
+fi
